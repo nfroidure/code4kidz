cceaf5e6177796ac5f2001d34ed98960ecf46823
nfroidure <nicolas.froidure@gmail.com>
2025-06-18 10:48:18 +0000

    Deploying to gh-pages from @ nfroidure/code4kidz@3c4171de4e971d4be3e2f357eda71948fc016db6 🚀

---
+++ _next/static/ipWMwwxLh3h8xYh9lGG1V/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]\u002F[[...slug]]","\u002F[locale]\u002Flearn","\u002F[locale]\u002Flearn\u002F[creature]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
